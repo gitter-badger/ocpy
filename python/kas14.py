96099e485b64d83ae8bc53759ffa5cd08074c27e
Jordan Matelsky <j6k4m8@gmail.com>
2015-08-04 13:51:03 -0400

    Python init

---
+++ python/kas14.py
@@ -0,0 +1,9 @@
+import ocp_access
+
+ocp_access.get_data(
+        token =     "kasthuri11",
+        x_lo =      0,              x_hi =      100,
+        y_lo =      0,              y_hi =      100,
+        z_lo =      1,              z_hi =      10
+)
+
---
+++ python/ocp_access.py
@@ -0,0 +1,100 @@
+import requests
+
+
+DEFAULT_SERVER =    'http://openconnecto.me'
+DEFAULT_FORMAT =    'hdf5'
+DEFAULT_ZOOM   =    1
+
+
+def get_data(token, 
+             x_lo, x_hi,
+             y_lo, y_hi,
+             z_lo, z_hi,
+             fmt=DEFAULT_FORMAT,
+             zoom=DEFAULT_ZOOM,
+             server=DEFAULT_SERVER,
+             location="./"):
+    """
+    Get data from the OCP server.
+    server: Internet-facing server
+    token:  Token to identify data to download
+    fmt:    The desired output format (see ocp_Convert repository to convert locally)
+    zoom:   Zoom level (starts at 0)
+    Q_lo:   The lower bound of dimension 'Q'
+    Q_hi:   The upper bound of dimension 'Q'
+    """
+
+    # The array of local files that we create
+    local_files = []
+
+    # OCP stores cubes of z-size = 16. To be efficient,
+    # we'll download in 16-z-slice units.
+    if z_hi - z_lo <= 16:
+        # We don't have to split, just download.
+        local_files.append(
+            _download_data(server, token, fmt, zoom,
+                            x_lo, x_hi,
+                            y_lo, y_hi,
+                            z_lo, z_hi, location)
+        )
+    else:
+        # We need to split into 16-slice chunks.
+        z_last = z_lo
+        while z_last < z_hi:
+            # Download from z_last to z_last + 16 OR
+            # z_hi, whichever is smaller
+            if z_hi <= z_last + 16:
+                # Download from z_last to z_hi
+                local_files.append(
+                    _download_data(server, token, fmt, zoom,
+                            x_lo, x_hi,
+                            y_lo, y_hi,
+                            z_last, z_hi, location)
+                )
+            else:
+                # Download from z_last to z_last + 16
+                local_files.append(
+                    _download_data(server, token, fmt, zoom,
+                            x_lo, x_hi,
+                            y_lo, y_hi,
+                            z_last, z_last + 16, location)
+                )
+
+            z_last += 16
+
+    # We now have an array, `local_files`, holding all of the
+    # files that we downloaded.
+
+    print [i for i in local_files]
+
+def _download_data(server, token, fmt, zoom, x_lo, x_hi, y_lo, y_hi, z_lo, z_hi, location):
+    """
+    Download the actual data from the server. Uses 1MB chunks when saving.
+    Returns the filename stored locally. Specify a save-location target in get_data.
+    """
+
+    # Build a string that holds the full URL to request.
+
+    request_data = [
+        server, 'ocp', 'ca',        # Boilerplate server URL
+        token, fmt, str(zoom),      # Set token, format, and zoom
+        str(x_lo) + "," + str(x_hi),# Z
+        str(y_lo) + "," + str(y_hi),# Y
+        str(z_lo) + "," + str(z_hi),# Z
+        ""                          # Trailing '/'
+    ]
+
+    request_url = '/'.join(request_data)
+    file_name   = '-'.join(request_data[3:-1]) + "." + fmt
+
+    # Create a `requests` object.
+    req = requests.get(request_url, stream=True)
+    # Now download (chunking to 1024 bytes from the stream)
+    with open(file_name, 'wb+') as f:
+        for chunk in req.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+                f.flush()
+
+    return file_name
+
