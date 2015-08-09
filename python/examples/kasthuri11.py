c15af59ab20109efd967e1b7ad0013f93349e0d6
Jordan Matelsky <j6k4m8@gmail.com>
2015-08-09 19:22:37 -0400

    Fix demo data, start convert.py

--- python/examples/kasthuri11.py
+++ python/examples/kasthuri11.py
@@ -1,17 +1,22 @@
 import ocpaccess.download
 
+# A few hundred megabytes of the kasthuri11 dataset.
 ocpaccess.download.get_data(
         token =     "kasthuri11",
         x_lo =      5000,              x_hi =      5500,
         y_lo =      5000,              y_hi =      5500,
         z_lo =      1,                 z_hi =      3,
-        location =  "data"
+        location =  "sample_data"
 )
 
+
+# The code below downloads the full kasthuri11 dataset.
 """
-ocpaccess.get_data(
-    x_lo =      0,              x_hi =      10752,
-    y_lo =      0,              y_hi =      13312,
-    z_lo =      1,              z_hi =      1850,
+ocpaccess.download.get_data(
+        token =     "kasthuri11",
+        x_lo =      0,              x_hi =      10752,
+        y_lo =      0,              y_hi =      13312,
+        z_lo =      1,              z_hi =      1850,
+        location =  "data"
 )
 """
---
+++ python/ocpaccess/convert/convert.py
@@ -0,0 +1,76 @@
+import os
+
+FILE_FORMATS = {
+    # Format.lower()    # File extension. By convention, use the first by default
+    'hdf5':             ['hdf5', 'h5', 'hdf'],
+    'tiff':             ['tiff', 'tif'],
+    'ramon':            ['m'],
+    'matlab':           ['m', 'mat'],
+}
+
+
+# This is not simply FILE_FORMATS in 'reverse', because while we may be able
+# to get the file extension for a RAMON object (.m) we can't guess a datatype
+# from the ambiguous extension ".m".
+
+
+def _get_extension_for_format(fmt):
+    """
+    Get the appropriate file extension for a given format.
+
+    Arguments:
+        fmt:        The format to find an extension for
+
+    Returns:
+        String. The format (without leading period),
+                or False if none was found.
+    """
+    if fmt in FILE_FORMATS:
+        # Use first file extension by default
+        return FILE_FORMATS[fmt][0]
+
+    # Otherwise, we don't recognize the format...
+    return False
+
+
+
+def _guess_format_from_extension(ext):
+    """
+    Guess the appropriate data type from file extension.
+
+    Arguments:
+        ext:        The file extension (period optional)
+
+    Returns:
+        String. The format (without leading period),
+                or False if none was found.
+    """
+    ext = ext.strip('.')
+
+    # We look through FILE_FORMATS for this extension.
+    # - If it appears zero times, return False. We can't guess.
+    # - If it appears once, we can simply return that format.
+    # - If it appears more than once, we can't guess (it's ambiguous,
+    #   e.g .m = RAMON or MATLAB)
+    
+
+
+
+def convert(in_file, out_file, in_fmt="", out_fmt=""):
+    """
+    Converts in_file to out_file, guessing datatype in the absence of
+    in_fmt and out_fmt.
+
+    Arguments:
+        in_file:    The name of the (existing) datafile to read
+        out_file:   The name of the file to create with converted data
+        in_fmt:     Optional. The format of incoming data, if not guessable
+        out_fmt:    Optional. The format of outgoing data, if not guessable
+
+    Returns:
+        String. Output filename
+    """
+
+    if in_fmt == "":
+        # Guess in_fmt from in_file
+        in_fmt = in_file.slice('.')[-1]
