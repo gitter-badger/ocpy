890bc58f29cd454ec4a01f8d5748b85afb472b84
Jordan Matelsky <j6k4m8@gmail.com>
2015-08-11 17:34:04 -0400

    Fix bock11 example coords

--- packages/python/examples/bock11_download.py
+++ packages/python/examples/bock11_download.py
@@ -5,8 +5,8 @@ This script downloads 1GB of the bock11 dataset from OCP.
 import ocpaccess.download
 
 downloaded_files = ocpaccess.download.get_data(
-                            token="bock11",         resolution=1,
-                            x_start=4000,           x_stop=6000,
-                            y_start=4000,           y_stop=6000,
+                            token="bock11",        resolution=4,
+                            x_start=3000,           x_stop=5000,
+                            y_start=3000,           y_stop=5000,
                             z_start=3200,           z_stop=3450,
                             location="bock11_sample")
