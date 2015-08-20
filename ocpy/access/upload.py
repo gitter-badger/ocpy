62670e1b59bd0474b3c5c3b1bf24d81aafaaa988
Jordan Matelsky <j6k4m8@gmail.com>
2015-08-19 23:31:57 -0400

    Improve upload bounds-guessing

--- ocpy/access/upload.py
+++ ocpy/access/upload.py
@@ -10,10 +10,14 @@ from enums import *
 from Request import *
 
 
-def put_data(token, data, x_start, x_stop, y_start, y_stop, z_start, z_stop, filename):
+def put_data(token, data, x_start, x_stop=0, y_start, y_stop=0, z_start, z_stop=0, filename):
     """
     Upload data onto the OCP server.
     """
+
+    if x_stop == 0: x_stop = x_start + data.shape[0]
+    if y_stop == 0: y_stop = y_start + data.shape[1]
+    if z_stop == 0: z_stop = z_start + data.shape[2]
 
     if (x_stop - x_start) != data.shape[0]:
         raise DataSizeError("Bad fit: x-range")
