b96f92f5f789a5d8e09568e11b6ec520d1b48cb7
Jordan Matelsky <j6k4m8@gmail.com>
2015-08-19 23:20:05 -0400

    DataSizeError exception

--- ocpy/access/enums.py
+++ ocpy/access/enums.py
@@ -2,3 +2,10 @@ DEFAULT_PROTOCOL =  'http'
 DEFAULT_SERVER =    'http://openconnecto.me'
 DEFAULT_FORMAT =    'hdf5'
 CHUNK_DEPTH    =    16
+
+class DataSizeError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
