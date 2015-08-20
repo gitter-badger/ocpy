56ed46bcb3048267d80ec005bc92f7b684cc4d9c
Jordan Matelsky <j6k4m8@gmail.com>
2015-08-19 22:51:09 -0400

    Add put_data

---
+++ ocpy/access/upload.py
@@ -0,0 +1,46 @@
+from __future__ import print_function
+import requests
+import h5py
+import os
+import numpy
+from PIL import Image
+import time
+
+from enums import *
+from Request import *
+
+
+def put_data(token, data, x_start, x_stop, y_start, y_stop, z_start, z_stop, filename):
+    """
+    Upload data onto the OCP server.
+    """
+
+    if (x_stop - x_start) != data.shape[0]:
+        raise DataSizeError("Bad fit: x-range")
+    if (y_stop - y_start) != data.shape[1]:
+        raise DataSizeError("Bad fit: y-range")
+    if (z_stop - z_start) != data.shape[2]:
+        raise DataSizeError("Bad fit: z-range")
+
+    fout = h5py.File(filename, driver="core", backing_store=True)
+    fout.create_dataset("CUTOUT", data.shape, data.dtype, compression="gzip", data=data)
+    fout.close()
+
+    req = Request(
+        token =         token,
+        x_start =       x_start,
+        x_stop =        x_stop,
+        y_start =       y_start,
+        y_stop =        y_stop,
+        z_start =       z_start,
+        z_stop =        z_stop,
+        resolution =    "1",
+        format =        "hdf5"
+    )
+
+    url = req.to_url()
+
+    with open(filename, 'rb') as payload:
+        req = requests.post(url, data=payload)
+
+    return req.status_code == 200
